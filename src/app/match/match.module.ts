50cb9c8c975c1c7b06e95b059faaaf3c0e80ef06
Sam Doidge <sam@samdoidge.com>
2020-02-09 18:29:10 +0000

    Create initial match-form sub-component.

--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -6,10 +6,11 @@ import { MatchComponent } from './match.component';
 import { MatchListComponent } from './match-list/match-list.component';
 import { MatchSummaryComponent } from './match-summary/match-summary.component';
 import { MatchDetailComponent } from './match-detail/match-detail.component';
+import { MatchFormComponent } from './match-form/match-form.component';
 
 
 @NgModule({
-  declarations: [MatchComponent, MatchListComponent, MatchSummaryComponent, MatchDetailComponent],
+  declarations: [MatchComponent, MatchListComponent, MatchSummaryComponent, MatchDetailComponent, MatchFormComponent],
   imports: [
     CommonModule,
     MatchRoutingModule
