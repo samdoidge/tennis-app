cedbaacbba8a18067c5342697fa296d5bf74d854
Sam Doidge <sam@samdoidge.com>
2020-02-09 18:27:54 +0000

    Create initial match sub-components.

---
+++ src/app/match/match-summary/match-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-match-summary',
+  templateUrl: './match-summary.component.html',
+  styleUrls: ['./match-summary.component.scss']
+})
+export class MatchSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MatchRoutingModule } from './match-routing.module';
 import { MatchComponent } from './match.component';
+import { MatchListComponent } from './match-list/match-list.component';
+import { MatchSummaryComponent } from './match-summary/match-summary.component';
+import { MatchDetailComponent } from './match-detail/match-detail.component';
 
 
 @NgModule({
-  declarations: [MatchComponent],
+  declarations: [MatchComponent, MatchListComponent, MatchSummaryComponent, MatchDetailComponent],
   imports: [
     CommonModule,
     MatchRoutingModule
